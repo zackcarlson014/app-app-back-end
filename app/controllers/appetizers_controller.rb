c796229b6dc4cbaa6a7a09f5d0903555b95328f6
ZaQuarius15 <jzcarlson014@gmail.com>
2020-11-09 16:42:45 -0600

    working on dat backend

--- app/controllers/appetizers_controller.rb
+++ app/controllers/appetizers_controller.rb
@@ -1,4 +1,17 @@
 class AppetizersController < ApplicationController
     def index
+        appetizers = Appetizer.all 
+        render json: appetizers, include: [:favorites]
+    end
+
+    def create
+        appetizer = Appetizer.create(appetizer_params)
+        render json: appetizer
+    end
+
+    private
+
+    def appetizer_params
+        params.require(:appetizer).permit(:title, :image_src, :rating)cd
     end
 end
