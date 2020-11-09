0cc118910793560444aaed68efcfca50e38ca139
tincogs <tomoday91@gmail.com>
2020-11-09 16:42:43 -0600

    api seed data

--- db/migrate/20201109191423_create_appetizers.rb
+++ db/migrate/20201109191423_create_appetizers.rb
@@ -2,8 +2,7 @@ class CreateAppetizers < ActiveRecord::Migration[6.0]
   def change
     create_table :appetizers do |t|
       t.string :title
-      t.integer :prep_time
-      t.integer :cook_time
+      t.string :image_src
       t.integer :rating
 
       t.timestamps
