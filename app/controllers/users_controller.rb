3c9344edceb80bd2ae53a00b9c0857a0f452373b
ZaQuarius15 <jzcarlson014@gmail.com>
2020-11-10 11:49:08 -0600

    additional controller testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all 
+        render json: users, include: :favorites
+    end
+
     def create
         user = User.find_or_create_by(user_params)
         render json: user
