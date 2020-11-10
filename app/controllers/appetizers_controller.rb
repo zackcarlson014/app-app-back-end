2c3e8d41cc976c7c6f1b23d1e3de19fd35c1b1d8
ZaQuarius15 <jzcarlson014@gmail.com>
2020-11-10 10:32:46 -0600

    appetizer controller actions modified

--- app/controllers/appetizers_controller.rb
+++ app/controllers/appetizers_controller.rb
@@ -1,7 +1,13 @@
 class AppetizersController < ApplicationController
     def index
         appetizers = Appetizer.all
-        render json: appetizers, include: [:favorites]
+        render json: appetizers, include: [:favorites], except: [:created_at, :updated_at]
+    end
+
+    def show 
+        appetizer = Appetizer.find(params[:id])
+        render json: appetizer, include: [:favorites], except: [:created_at, :updated_at]
+        
     end
 
     def create
