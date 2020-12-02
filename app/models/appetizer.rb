69925c30fc34b50dc1070180fb7454b093f131c3
tincogs <tomoday91@gmail.com>
2020-12-02 12:32:25 -0600

    fix appetizer model relationship

--- app/models/appetizer.rb
+++ app/models/appetizer.rb
@@ -1,7 +1,7 @@
 class Appetizer < ApplicationRecord
     has_many :favorites
-    has_many :appetizers, through: :favorites
+    has_many :users, through: :favorites
 
     validates_uniqueness_of :title
-    
+
 end
